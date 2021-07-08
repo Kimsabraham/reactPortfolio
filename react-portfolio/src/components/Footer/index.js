83d2706411eeb4998e1d9cc065fbeca8a718d21c
Abraham Kim <krnpride730@yahoo.com>
2021-07-08 19:03:51 -0400

    added footer

---
+++ react-portfolio/src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer bg-primary fixed-bottom">
+      <a href="https://github.com/Kimsabraham">
+        <img
+          src="https://img.icons8.com/color/48/000000/github--v1.png"
+          alt="Github"
+          className="icon"
+        />
+      </a>
+      <a href="https://www.linkedin.com/in/abraham-kim-659578156//">
+        <img
+          src="https://img.icons8.com/fluent/48/000000/linkedin.png"
+          alt="LinkedIn"
+          className="icon"
+        />
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
